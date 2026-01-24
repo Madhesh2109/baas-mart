8f979d5b2a304e0a7c31fc9e91fc9a632849ac6d
Madhesh <madhesh.g.2109@gmail.com>
2026-01-24 18:38:26 +0530

    Added products

--- src/components/ProductReveal.jsx
+++ src/components/ProductReveal.jsx
@@ -2,6 +2,12 @@ import "./ProductReveal.css";
 import sambarPowder from "../assets/sambar.png";
 import rasamPowder from "../assets/rasam.png";
 import chilliPowder from "../assets/chilli.png";
+import briyaniPowder from "../assets/briyani.jpg";
+import idlyChilliPowder from "../assets/idly.jpeg";
+import kulambuPowder from "../assets/kulambu.jpg";
+import kariMasalaPowder from "../assets/kari-masala.png";
+import chickenGravyPowder from "../assets/chicken-gravy.png";
+import chicken65Powder from "../assets/chicken-65.png";
 
 export default function ProductReveal() {
   return (
@@ -40,6 +46,66 @@ export default function ProductReveal() {
             <p>
               Made from carefully dried chillies,
               ground fresh in small quantities.
+            </p>
+          </div>
+
+          <div className="product-block">
+            <div className="product-image">
+              <img src={briyaniPowder} alt="Briyani Masala Powder" />
+            </div>
+            <h3>Briyani Masala Powder</h3>
+            <p>
+              Traditional spice blend for rich and aromatic briyani.
+            </p>
+          </div>
+
+          <div className="product-block">
+            <div className="product-image">
+              <img src={idlyChilliPowder} alt="Idly Chilli Powder" />
+            </div>
+            <h3>Idly Chilli Powder</h3>
+            <p>
+              Perfect spicy companion for idly and dosa.
+            </p>
+          </div>
+
+          <div className="product-block">
+            <div className="product-image">
+              <img src={kulambuPowder} alt="Kulambu Powder" />
+            </div>
+            <h3>Kulambu Powder</h3>
+            <p>
+              Authentic South Indian kulambu masala blend.
+            </p>
+          </div>
+          
+          <div className="product-block">
+            <div className="product-image">
+              <img src={kariMasalaPowder} alt="Kari Masala Powder" />
+            </div>
+            <h3>Kari Masala Powder</h3>
+            <p>
+              All-purpose masala for everyday cooking.
+            </p>
+          </div>
+
+          <div className="product-block">
+            <div className="product-image">
+              <img src={chickenGravyPowder} alt="Chicken Gravy Powder" />
+            </div>
+            <h3>Chicken Gravy Powder</h3>
+            <p>
+              Rich blend for delicious chicken gravies.
+            </p>
+          </div>
+
+          <div className="product-block">
+            <div className="product-image">
+              <img src={chicken65Powder} alt="Chicken 65 Powder" />
+            </div>
+            <h3>Chicken 65 Powder</h3>
+            <p>
+              Crispy, spicy masala mix for Chicken 65.
             </p>
           </div>
         </div>
